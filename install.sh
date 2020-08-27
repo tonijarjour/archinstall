8d1eb6c47b1c8f02a4ccf0f7a22ada9261afdadd
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2020-08-27 10:10:39 -0400

    New mirror list

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 cat > /etc/pacman.d/mirrorlist << EOF
-Server = https://arch.mirror.constant.com/\$repo/os/\$arch
-Server = http://mirror.stephen304.com/archlinux/\$repo/os/\$arch
-Server = http://arch.mirror.constant.com/\$repo/os/\$arch
-Server = http://arch.mirror.square-r00t.net/\$repo/os/\$arch
-Server = http://mirror.wdc1.us.leaseweb.net/archlinux/\$repo/os/\$arch
-Server = http://ftp.osuosl.org/pub/archlinux/\$repo/os/\$arch
-Server = http://mirrors.advancedhosters.com/archlinux/\$repo/os/\$arch
+Server = https://mirror.osbeck.com/archlinux/\$repo/os/\$arch
+Server = https://iad.mirrors.misaka.one/archlinux/\$repo/os/\$arch
+Server = https://arch.mirror.square-r00t.net/\$repo/os/\$arch
+Server = https://plug-mirror.rcac.purdue.edu/archlinux/\$repo/os/\$arch
+Server = https://mirror.sergal.org/archlinux/\$repo/os/\$arch
+Server = https://mirrors.lug.mtu.edu/archlinux/\$repo/os/\$arch
+Server = https://repo.ialab.dsu.edu/archlinux/\$repo/os/\$arch
+Server = https://ftp.sudhip.com/archlinux/\$repo/os/\$arch
+Server = https://arlm.tyzoid.com/\$repo/os/\$arch
+Server = https://arch.hu.fo/archlinux/\$repo/os/\$arch
+Server = https://mirrors.melbourne.co.uk/archlinux/\$repo/os/\$arch
+Server = https://archmirror1.octyl.net/\$repo/os/\$arch
+Server = https://mirror.wtnet.de/arch/\$repo/os/\$arch
+Server = https://mirror.pkgbuild.com/\$repo/os/\$arch
+Server = https://archlinux.thaller.ws/\$repo/os/\$arch
 EOF
 
 pacstrap /mnt base linux linux-firmware
