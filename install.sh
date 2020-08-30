8b9afaa29b6383f5c5704d3d8c08f1291c6d857c
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2020-08-30 05:19:03 -0400

    install simplified further

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-USERNAME='toni'
 HOSTNAME='sakura'
-EDITOR='nvim'
 TIMEZONE='America/New_York'
 LOCALE='en_US'
 CPUCODE='intel'
-NETWORKINF='eno1'
-PATHTOBOOT='boot'
+ESPBOOT='boot'
 ROOTLABEL='Arch Linux'
-USERSHELL='bash'
 
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
@@ -22,43 +18,19 @@ cat > /etc/hosts << EOF
 ::1             localhost
 127.0.1.1       $HOSTNAME.localdomain $HOSTNAME
 EOF
-cat > /etc/pacman.d/mirrorlist << EOF
-Server = https://arch.mirror.square-r00t.net/\$repo/os/\$arch
-Server = https://arch.mirror.constant.com/\$repo/os/\$arch
-EOF
-pacman -S ntfs-3g intel-ucode man-db man-pages systemd-swap opendoas
-useradd -m -g wheel -s /bin/$USERSHELL $USERNAME
-echo "permit nopass :wheel" > /etc/doas.conf
-cat > /etc/security/access.conf << EOF
-+:root:LOCAL
-+:(wheel):LOCAL
--:ALL:ALL
-EOF
+pacman -S intel-ucode
 bootctl install
-cat > /$PATHTOBOOT/loader/loader.conf << EOF
+cat > /$ESPBOOT/loader/loader.conf << EOF
 default         arch
 timeout         3
 editor          no
 EOF
-cat > /$PATHTOBOOT/loader/entries/arch.conf << EOF
+cat > /$ESPBOOT/loader/entries/arch.conf << EOF
 title   $ROOTLABEL
 linux   /vmlinuz-linux
 initrd  /$CPUCODE-ucode.img
 initrd  /initramfs-linux.img
 options root="LABEL=$ROOTLABEL" rw
 EOF
-echo "/dev/sda1	/mnt/archive	ntfs-3g	uid=$USERNAME,gid=wheel,umask=0022 0 0" >> /etc/fstab
-mkdir /mnt/archive
-cat > /etc/systemd/network/20-wired.network << EOF
-[Match]
-Name=$NETWORKINF
-
-[Network]
-DHCP=yes
-EOF
-echo "swapfc_enabled=1" > /etc/systemd/swap.conf
-systemctl enable systemd-networkd.service
-systemctl enable systemd-resolved.service
-systemctl enable systemd-swap
 echo "-- ALL STEPS FINISHED"
-echo "-- Remember to set a password for root and $USERNAME"
+echo "-- Remember to set a PASSWORD for root"
