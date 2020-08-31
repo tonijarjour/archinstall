cdb2ba7404de5c9a8929c59091c9d38fc50d3a87
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2020-08-31 01:23:15 -0400

    moving to one file

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+USERNAME='toni'
 HOSTNAME='sakura'
+USERSHELL='bash'
 TIMEZONE='America/New_York'
 LOCALE='en_US'
 CPUCODE='intel'
 ESPBOOT='boot'
 ROOTLABEL='Arch Linux'
+INTERFACE='eno1'
 
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
@@ -17,6 +20,14 @@ cat > /etc/hosts << EOF
 127.0.0.1       localhost
 ::1             localhost
 127.0.1.1       $HOSTNAME.localdomain $HOSTNAME
+EOF
+useradd -m -g wheel -s /bin/$USERSHELL $USERNAME
+echo "permit nopass :wheel" > /etc/doas.conf
+echo "/dev/sda1 /mnt/archive ntfs-3g uid=$USERNAME,gid=wheel,umask=0022 0 0" >> /etc/fstab
+cat > /etc/security/access.conf << EOF
++:root:LOCAL
++:(wheel):LOCAL
+-:ALL:ALL
 EOF
 bootctl install
 cat > /$ESPBOOT/loader/loader.conf << EOF
@@ -31,5 +42,14 @@ initrd  /$CPUCODE-ucode.img
 initrd  /initramfs-linux.img
 options root="LABEL=$ROOTLABEL" rw
 EOF
+cat > /etc/systemd/network/20-wired.network << EOF
+[Match]
+Name=$INTERFACE
+
+[Network]
+DHCP=yes
+EOF
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
 echo "-- ALL STEPS FINISHED"
-echo "-- Remember to set a PASSWORD for root"
+echo "-- Remember to set a PASSWORD for root and $USERNAME"
