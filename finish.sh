651cc89ae84bc31efd2b03e55b93b964f8bdb1b7
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2020-08-30 23:25:16 -0400

    no swap file

--- finish.sh
+++ finish.sh
@@ -20,9 +20,7 @@ cat > /etc/pacman.d/mirrorlist << EOF
 Server = https://arch.mirror.square-r00t.net/\$repo/os/\$arch
 Server = https://arch.mirror.constant.com/\$repo/os/\$arch
 EOF
-pacman -S ntfs-3g systemd-swap opendoas
+pacman -S ntfs-3g opendoas
 echo "permit nopass :wheel" > /etc/doas.conf
-echo "swapfc_enabled=1" > /etc/systemd/swap.conf
-systemctl enable --now systemd-swap
 useradd -m -g wheel -s /bin/$USERSHELL $USERNAME
 echo "/dev/sda1 /mnt/archive ntfs-3g uid=$USERNAME,gid=wheel,umask=0022 0 0" >> /etc/fstab
